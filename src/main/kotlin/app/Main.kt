748d15f7bc42b5608c0ef7155677470b35cae963
PatEvs <ppevans11@gmail.com>
2019-10-21 20:17:19 +1300

    ✨  src

---
+++ src/main/kotlin/app/Main.kt
@@ -0,0 +1,62 @@
+package app
+
+import app.util.HerokuUtil
+import app.util.RateLimitUtil
+import io.javalin.Javalin
+import io.javalin.core.compression.Brotli
+import io.javalin.core.compression.Gzip
+import io.javalin.core.util.Header
+import io.javalin.http.BadRequestResponse
+import io.javalin.http.NotFoundResponse
+import io.javalin.plugin.rendering.vue.VueComponent
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.ServerConnector
+import org.eclipse.jetty.util.thread.QueuedThreadPool
+import org.slf4j.LoggerFactory
+
+fun main() {
+
+    val log = LoggerFactory.getLogger("app.MainKt")
+    val app = Javalin.create {
+        it.addStaticFiles("/public")
+        it.compressionStrategy(Brotli(), Gzip())
+        it.server {
+            Server(QueuedThreadPool(200, 8, 120000)).apply {
+                connectors = arrayOf(ServerConnector(server).apply {
+                    port = Config.getPort() ?: 7070
+                    idleTimeout = 120_000
+                })
+            }
+        }
+    }.apply {
+        get("/api/can-load") { ctx ->
+            val user = ctx.queryParam<String>("user").get()
+            if (!UserService.userExists(user)) throw NotFoundResponse()
+            ctx.status(if (UserService.canLoadUser(user)) 200 else 400)
+        }
+        get("/api/user/:user") { ctx ->
+            val user = ctx.pathParam("user")
+            if (!UserService.userExists(user)) throw NotFoundResponse()
+            if (!UserService.canLoadUser(user)) throw BadRequestResponse("Can't load user")
+            ctx.json(UserService.getUserProfile(user))
+        }
+        get("/search", VueComponent("<search-view></search-view>"))
+        get("/user/:user", VueComponent("<user-view></user-view>"))
+        ws("/rate-limit-status") { ws ->
+            ws.onConnect { GhService.registerClient(it) }
+            ws.onClose { GhService.unregisterClient(it) }
+            ws.onError { GhService.unregisterClient(it) }
+        }
+        after { it.cookie("gtm-id", Config.getGtmId() ?: "") }
+    }.exception(Exception::class.java) { e, ctx ->
+        log.warn("Uncaught exception", e)
+        ctx.status(500)
+    }.error(404) {
+        if (it.header(Header.ACCEPT)?.contains("text/html") == true) it.redirect("/search")
+    }.start()
+
+    HerokuUtil.enableSslRedirect(app)
+    RateLimitUtil.enableTerribleRateLimiting(app)
+    UserService.syncWatchers()
+
+}
---
+++ src/main/kotlin/app/UserService.kt
@@ -0,0 +1,114 @@
+package app
+
+import app.util.CommitCountUtil
+import org.eclipse.egit.github.core.Repository
+import org.eclipse.egit.github.core.RepositoryCommit
+import org.eclipse.egit.github.core.User
+import org.slf4j.LoggerFactory
+import java.io.Serializable
+import java.time.Instant
+import java.util.concurrent.ConcurrentHashMap
+import java.util.stream.IntStream
+import kotlin.streams.toList
+
+object UserService {
+
+    private const val pageSize = 100
+    private val log = LoggerFactory.getLogger("app.UserCtrlKt")
+    private val repo = GhService.repos.getRepository("tipsy", "profile-summary-for-github")
+    private val watchers = ConcurrentHashMap.newKeySet<String>()
+    private val freeRequestCutoff = Config.freeRequestCutoff()
+
+    fun userExists(user: String): Boolean = try {
+        GhService.users.getUser(user) != null
+    } catch (e: Exception) {
+        false
+    }
+
+    fun canLoadUser(user: String): Boolean {
+        val remainingRequests by lazy { GhService.remainingRequests }
+        val hasFreeRemainingRequests by lazy { remainingRequests > (freeRequestCutoff ?: remainingRequests) }
+        return Config.unrestricted()
+                || Cache.contains(user)
+                || hasFreeRemainingRequests
+                || (remainingRequests > 0 && hasStarredRepo(user))
+    }
+
+    fun getUserProfile(username: String): UserProfile {
+        if (Cache.invalid(username)) {
+            val user = GhService.users.getUser(username)
+            val repos = GhService.repos.getRepositories(username).filter { !it.isFork && it.size != 0 }
+            val repoCommits = repos.parallelStream().map { it to commitsForRepo(it).filter { it.author?.login.equals(username, ignoreCase = true) } }.toList().toMap()
+            val langRepoGrouping = repos.groupingBy { (it.language ?: "Unknown") }
+
+            val quarterCommitCount = CommitCountUtil.getCommitsForQuarters(user, repoCommits)
+            val langRepoCount = langRepoGrouping.eachCount().toList().sortedBy { (_, v) -> -v }.toMap()
+            val langStarCount = langRepoGrouping.fold(0) { acc, repo -> acc + repo.watchers }.toList().sortedBy { (_, v) -> -v }.toMap()
+            val langCommitCount = langRepoGrouping.fold(0) { acc, repo -> acc + repoCommits[repo]!!.size }.toList().sortedBy { (_, v) -> -v }.toMap()
+            val repoCommitCount = repoCommits.map { it.key.name to it.value.size }.toList().sortedBy { (_, v) -> -v }.take(10).toMap()
+            val repoStarCount = repos.filter { it.watchers > 0 }.map { it.name to it.watchers }.sortedBy { (_, v) -> -v }.take(10).toMap()
+
+            val repoCommitCountDescriptions = repoCommitCount.map { it.key to repos.find { r -> r.name == it.key }?.description }.toMap()
+            val repoStarCountDescriptions = repoStarCount.map { it.key to repos.find { r -> r.name == it.key }?.description }.toMap()
+
+            Cache.putUserProfile(UserProfile(
+                    user,
+                    quarterCommitCount,
+                    langRepoCount,
+                    langStarCount,
+                    langCommitCount,
+                    repoCommitCount,
+                    repoStarCount,
+                    repoCommitCountDescriptions,
+                    repoStarCountDescriptions
+            ))
+        }
+        return Cache.getUserProfile(username)!!
+    }
+
+    private fun hasStarredRepo(username: String): Boolean {
+        val login = username.toLowerCase()
+        if (watchers.contains(login)) return true
+        syncWatchers()
+        return watchers.contains(login)
+    }
+
+    fun syncWatchers() {
+        val realWatchers = repo.watchers
+        if (watchers.size < realWatchers) {
+            val startPage = watchers.size / pageSize + 1
+            val lastPage = realWatchers / pageSize + 1
+            if (startPage == lastPage)
+                addAllWatchers(lastPage)
+            else
+                IntStream.rangeClosed(startPage, lastPage).parallel().forEach { page -> addAllWatchers(page) }
+        }
+    }
+
+    private fun addAllWatchers(pageNumber: Int) = try {
+        GhService.watchers.pageWatchers(repo, pageNumber, pageSize).first().forEach { watchers.add(it.login.toLowerCase()) }
+    } catch (e: Exception) {
+        log.info("Exception while adding watchers", e)
+    }
+
+    private fun commitsForRepo(repo: Repository): List<RepositoryCommit> = try {
+        GhService.commits.getCommits(repo)
+    } catch (e: Exception) {
+        listOf()
+    }
+
+}
+
+data class UserProfile(
+        val user: User,
+        val quarterCommitCount: Map<String, Int>,
+        val langRepoCount: Map<String, Int>,
+        val langStarCount: Map<String, Int>,
+        val langCommitCount: Map<String, Int>,
+        val repoCommitCount: Map<String, Int>,
+        val repoStarCount: Map<String, Int>,
+        val repoCommitCountDescriptions: Map<String, String?>,
+        val repoStarCountDescriptions: Map<String, String?>
+) : Serializable {
+    val timeStamp = Instant.now().toEpochMilli()
+}
